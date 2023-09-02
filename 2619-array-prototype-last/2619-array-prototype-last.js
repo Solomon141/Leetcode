ddd85c9cba983104cfe65f493eb8a34bf9911d65
Solomon141 <31657654+Solomon141@users.noreply.github.com>
2023-09-02 07:12:58 +0100

    Time: 61 ms (15.02%), Space: 41.1 MB (94.30%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,10 @@
+Array.prototype.last = function(nums) {
+    if(this.length > 0)
+        return this[this.length-1];
+    return -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
